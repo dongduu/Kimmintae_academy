c8804e0747eb211888e312fb29c67c462bb4f0f1
dongduu <4012popo@naver.com>
2021-10-12 17:34:37 +0900

    [김민태_프론트엔드 아카데미] add : 두 개의 화면을 가진 웹앱

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const contents = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENTS_URL = 'http://api.hnpwa.com/v0/item/@id.json';
 
 ajax.open('GET', NEWS_URL, false) // true: 비동기 처리 false: 동기 처리
 ajax.send();
@@ -7,10 +10,30 @@ ajax.send();
 const newsFead = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function () {
+    const id = location.hash.substr(1);
+
+    ajax.open('GET', CONTENTS_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+
+    contents.appendChild(title);
+});
+
 for (let i = 0; i < 10; i++) {
     const li = document.createElement('li')
-    li.innerHTML = newsFead[i].title;
+    const a = document.createElement('a');
+
+    a.href = `#${newsFead[i].id}`;
+    a.innerHTML = `${newsFead[i].title} (${newsFead[i].comments_count})`;
+    
+    li.appendChild(a);
     ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(contents);
