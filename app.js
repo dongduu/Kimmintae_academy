a52af0c68703b520855241e2565516de760769f6
dongduu <4012popo@naver.com>
2021-10-12 18:09:53 +0900

    [김민태_프론트엔드 아카데미] add : 문자열을 활용한 HTML 다루기

--- app.js
+++ app.js
@@ -4,36 +4,40 @@ const contents = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENTS_URL = 'http://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false) // true: 비동기 처리 false: 동기 처리
-ajax.send();
+function getData(url) {
+    ajax.open('GET', url, false) // true: 비동기 처리 false: 동기 처리
+    ajax.send();
 
-const newsFead = JSON.parse(ajax.response);
+    return JSON.parse(ajax.response);
+}
+
+const newsFead = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function () {
     const id = location.hash.substr(1);
 
-    ajax.open('GET', CONTENTS_URL.replace('@id', id), false);
-    ajax.send();
-
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENTS_URL.replace('@id', id));
     const title = document.createElement('h1');
-
+    
     title.innerHTML = newsContent.title;
 
     contents.appendChild(title);
 });
 
 for (let i = 0; i < 10; i++) {
-    const li = document.createElement('li')
-    const a = document.createElement('a');
+    const div = document.createElement('div');
 
-    a.href = `#${newsFead[i].id}`;
-    a.innerHTML = `${newsFead[i].title} (${newsFead[i].comments_count})`;
-    
-    li.appendChild(a);
-    ul.appendChild(li);
+    div.innerHTML = `
+        <li>
+            <a href = "#${newsFead[i].id}">
+            ${newsFead[i].title} (${newsFead[i].comments_count})
+            </a>
+        </li>
+    `;
+
+    ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
-container.appendChild(contents);
+container.appendChild(contents);    
