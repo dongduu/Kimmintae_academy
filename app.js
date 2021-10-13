04ad86a9a5a26ba07592b105c00209652b6655ca
dongduu <4012popo@naver.com>
2021-10-13 23:04:18 +0900

    [김민태_프론트엔드 아카데미] add : 페이징 구현하기

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const contents = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENTS_URL = 'http://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage: 1,
+};
 
 function getData(url) {
     ajax.open('GET', url, false) // true: 비동기 처리 false: 동기 처리
@@ -13,35 +16,39 @@ function getData(url) {
 
 function newsFeed () {
     const newsList = [];
-    const newsFead = getData(NEWS_URL);
+    const newsFeed = getData(NEWS_URL);
 
     newsList.push('<ul>');
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
             <li>
-                <a href = "#${newsFead[i].id}">
-                ${newsFead[i].title} (${newsFead[i].comments_count})
+                <a href = "#/show/${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
     `);
+    }    
     newsList.push('</ul>');
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+        </div>
+    `)
 
     container.innerHTML = newsList.join(''); // 합쳐주는 함수
-    }
 };
 
 function newsDetail () {
-    const id = location.hash.substr(1);
-
+    const id = location.hash.substr(7);
     const newsContent = getData(CONTENTS_URL.replace('@id', id));
-    const title = document.createElement('h1');
 
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
         
         <div>
-            <a href='#'>'목록으로'</a>
+            <a href='#/page/${store.currentPage}'>'목록으로'</a>
         </div>
     `;
 };
@@ -50,6 +57,9 @@ function router () {
     const routePath = location.hash; 
 
     if (routePath === '') { // 해시에 아무런 id가 없는 경우
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0) { // indexOf: 있으면 0 이상의 위치 값을 리턴, 없으면 -1을 리턴
+        store.currentPage = Number(substr(7));
         newsFeed();
     } else {
         newsDetail();
