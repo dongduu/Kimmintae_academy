74c7eb24a9dc8cae3eff183f18f138203c233e4f
dongduu <4012popo@naver.com>
2021-10-23 23:48:59 +0900

    [김민태_프론트엔드 아카데미] add : 참조사전-참조와 복사

--- basic/basic.js
+++ basic/basic.js
@@ -1,34 +1,23 @@
-const colors = ['red', 'yellow', 'blue'];
-
-const yellow = colors[1];
-const red = colors[0];
-const blue = colors[2];
-
-yellow;
-
-const [red, yellow, blue] = colors;
-
-yellow;
-
 let a = 10;
-let b = '10';
+let b = a;
 
-if (a == b) {
-    console.log('true');
-} else {
-    console.log('false')
-} // true
+b = 20;
 
-if (a === b) {
+let o = {
+    loading: false
+};
+
+
+function foo(a) {
+    o.loading = true;
+}
+
+foo(o);
+
+if (o.loading === true) {
     console.log('true');
 } else {
     console.log('false');
-} // false
-
-if (a === b) {
-    a = 0;
-} else {
-    a = 1;
 }
 
-a = (a === b) ? 0 : 1;
+console.log('done');
