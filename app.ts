07c467a34d0934521183a400bc94cc767341b708
dongduu <4012popo@naver.com>
2021-11-09 23:34:50 +0900

    [김민태_프론트엔드 아카데미] add : 참조사전 - 속성과 메소드

--- app.ts
+++ app.ts
@@ -1,67 +1,61 @@
-import * as allTypes from './basic/basic';
-
-const foo: allTypes.FooFunction = function(){
-    return '아무 쓸모 없는 함수';
+// 내용이 함수면 메소드 데이터면 속성
+type MyObject = {
+    name?: string;
+    age: number;
+    getFamilyName: () => string;
+    getLastName: () => string;
+    getBloodType: () => string;
 }
 
-const iUsers: allTypes.IUser = {
-    // id:'1',
-    id:1,
-    name: '빌 게이츠',
-    email: 'bill@ms.com',
-    receiveInfo: false,
-    active: 'Y',
-}
-
-const iUserProfile:allTypes.TUserProfile = {
-    id:1,
-    name: '빌 게이츠',
-    email: 'bill@ms.com',
-    receiveInfo: false,
-    active: 'Y',
-    profileImage: 'https://...',
-    github: 'okay',
+const obj: MyObject = {
+    name: 'dongdu',
+    age: 22,
+    getFamilyName: function () {
+        return 'Lee';
+    },
+    getLastName: () => 'Lee',
+    getBloodType() {
+        return 'A';
+    },
 };
 
-const IUserProfiles: allTypes.TUserProfile[] = [];
+obj.name;
+obj.age;
+obj.getFamilyName();
+obj.getBloodType();
 
-const iStyle: allTypes.IOnlyNumberValueObject = {
-    borderWidth: 5,
-    width: 300,
-    height: 100,
-}
+obj.age = 200;
+obj.age = -500;
 
-const TStyle: allTypes.TOnlyBooleanValueObject = {
-    border: true,
-    visible: false,
-    display:true,
-}
+class Person{
+    _bloodType: string;
 
-const getApi: allTypes.IGetApi = (url, search = '') => {
-    return new Promise(resolve => resolve('OK'))
-};
+    constructor(bloodType: string){
+        this._bloodType = bloodType;
+    }
 
-getApi('./api/users').then(data => console.log(data));
+    set bloodType (aType: string) {
+        if(aType === "A" || aType === "B" || aType === "AB" || aType === "O") {
+            this._bloodType = aType;
+        } // => 다른 값을 넣으면 아무것도 세팅이 안 됨
+    }
 
-class Rect implements allTypes.IRect {
-    id : number;
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-
-    constructor(x: number, y: number, width: number, height: number){
-        this.id = Math.random() * 100000;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    get bloodType() {
+        return `${this._bloodType} 형`;
     }
 }
 
-function createDefaultRect(cstor: allTypes.IRectConstruct){
-    return new cstor(0, 0, 100, 100);
-}
+const p1 = new Person('A');
 
-const rect1 = new Rect(0, 0, 100, 20);
-const rect2 = createDefaultRect(Rect);
+p1.bloodType; // = obj.getBloodType();
+p1.bloodType = 'C'; // 속성에 접근하는 것처럼 () 필요없음1! => 이건 메소드 형식(set 덕분)
+
+const myObj = Object.create(null, {
+    name: {
+        value: 'Lee DongDu',
+        writable: true, // true면 바꿀 수 있음 false면 못바꿈(readonly)
+        configurable: false, // 못지움 delete 사용 불가, true면 지울 수 있음
+    }
+})
+
+myObj.name;
