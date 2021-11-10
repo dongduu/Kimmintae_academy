d39783b64d4caee200761331a3f5c69267589913
dongduu <4012popo@naver.com>
2021-11-10 23:50:52 +0900

    [김민태_프론트엔드 아카데미] add : 참조사전-일급함수(1)

--- app.ts
+++ app.ts
@@ -1,61 +1,26 @@
-// 내용이 함수면 메소드 데이터면 속성
-type MyObject = {
-    name?: string;
-    age: number;
-    getFamilyName: () => string;
-    getLastName: () => string;
-    getBloodType: () => string;
+function ul(child: string): string {
+    return `<ul>${child}</ul>`;
 }
 
-const obj: MyObject = {
-    name: 'dongdu',
-    age: 22,
-    getFamilyName: function () {
-        return 'Lee';
-    },
-    getLastName: () => 'Lee',
-    getBloodType() {
-        return 'A';
-    },
-};
-
-obj.name;
-obj.age;
-obj.getFamilyName();
-obj.getBloodType();
-
-obj.age = 200;
-obj.age = -500;
-
-class Person{
-    _bloodType: string;
-
-    constructor(bloodType: string){
-        this._bloodType = bloodType;
-    }
-
-    set bloodType (aType: string) {
-        if(aType === "A" || aType === "B" || aType === "AB" || aType === "O") {
-            this._bloodType = aType;
-        } // => 다른 값을 넣으면 아무것도 세팅이 안 됨
-    }
-
-    get bloodType() {
-        return `${this._bloodType} 형`;
-    }
+function ol(child: string): string {
+    return `<ol>${child}</ol>`;
 }
 
-const p1 = new Person('A');
+function makeLI(
+    container: (child: string) => string,
+    contents: string[]
+): string {
+    const liList = [];
 
-p1.bloodType; // = obj.getBloodType();
-p1.bloodType = 'C'; // 속성에 접근하는 것처럼 () 필요없음1! => 이건 메소드 형식(set 덕분)
-
-const myObj = Object.create(null, {
-    name: {
-        value: 'Lee DongDu',
-        writable: true, // true면 바꿀 수 있음 false면 못바꿈(readonly)
-        configurable: false, // 못지움 delete 사용 불가, true면 지울 수 있음
+    for (const content of contents) {
+        liList.push(`<Li>${content}</Li>`)
     }
-})
 
-myObj.name;
+    return container(liList.join(''));
+}
+
+const htmlUL = makeLI(ul, ['월', '화', '수','목','금','토','일']);
+const htmlOL = makeLI(ol, ['봄', '여름', '가을', '겨울']);
+
+console.log(htmlUL);
+console.log(htmlOL);
