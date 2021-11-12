78e1d497fa58b673065224c143380ae4d65281a3
dongduu <4012popo@naver.com>
2021-11-12 21:19:52 +0900

    [김민태_프론트엔드 아카데미] add : 참조사전-생성기 함수

--- basic/basic.js
+++ basic/basic.js
@@ -1,22 +1,42 @@
-function salePrice(discountRate, price){
-    return price - (price * (discountRate * 0.01));
+function makeInfiniteEnergyGenerator(){
+    let energy = 0;
+    return function (booster = 0){
+        if(booster){
+            energy += booster;
+        } else {
+            energy++;
+        }
+
+        return energy;
+    };
 }
 
-console.log(salePrice(30, 567000));
-console.log(salePrice(10, 567000));
+function* infiniteEnergyGenerator() {
+    let energy = 1;
+    while(true){ // 무한 루프
+        const booster = yield energy;
 
-function discountPrice(discountRate){
-    return function(price){
-        return price - (price * (discountRate * 0.01));
-    }
+        if(booster){
+            energy += booster;
+        } else {
+            energy++;
+        }
+    } // return 없음
+}
+/*
+const energy = makeInfiniteEnergyGenerator();
+
+for(let i =0; i<5; i++){
+    console.log(energy());
 }
 
-console.log(discountPrice(30)(567000));
-console.log(discountPrice(10)(567000));
-
-let summerPrice = discountPrice(30);
-let winterPrice = discountPrice(10);
+console.log(energy(5));
+*/
+const energyGenerator = infiniteEnergyGenerator();
 
 
-console.log(summerPrice(567000));
-console.log(winterPrice(567000));
+for(let i =0; i<5; i++){
+    console.log(energyGenerator.next()); // 에너지제너레이터는 객체이기 때문에 메소드 사용
+}
+
+console.log(energyGenerator.next(5))
