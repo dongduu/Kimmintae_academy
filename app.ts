7088d876ee19650dc1dc870bc3ccd1c129b75fbd
dongduu <4012popo@naver.com>
2021-11-13 23:20:16 +0900

    [김민태_프론트엔드 아카데미] add : 참조사전-객체(데이터로서의 객체1)

--- app.ts
+++ app.ts
@@ -1,33 +1,63 @@
-function delay(ms:number) : Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        setTimeout(() => {
-            if(Math.floor(Math.random() *10) % 2 ===0){
-                resolve('success');
-            } else {
-                reject('failure');;
-            }
-        }, ms);
-    });
+// 객체 문법
+type Box = { // 타입 정의
+    width:number;
+    height:number;
+    borderRadius:number;
+    backgroundColor:string;
+    borderWidth?: number;
+    ['className']?: string; // 계산된 속성(속성명을 값으로 취급해서 기술할 수 있는 표기법)
 }
 
-// 비동기 프로그래밍 코드
-// then과 catch에 callback 함수를 전달해야함
-delay(3000)
-    .then((result: string) =>{
-        console.log('done promise' + result);
-    })
-    .catch((error: string) =>{
-        console.error('fail promise!' + error);
-    });
+let box: Box = { // 타입이 box인 객체
+    width:200,
+    height:200,
+    borderRadius:5,
+    backgroundColor:'red',
+}
+// => 이렇게 만드는 방법을 객체 리터럴이라고 함
 
-async function main(){
-    try{
-        console.log('start job')
-        const result = await delay(3000);
-        console.error('done async!' + result);
-    } catch(e) {
-        console.error('fail async' + e);
+// 함수 이용
+function makeBox(
+    width:number,
+    height:number,
+    borderRadius:number,
+    backgroundColor:string,
+) : Box {
+    return {
+        width,height,borderRadius,backgroundColor
+    };
+}
+
+makeBox(100,100, 0, 'blue');
+
+// 클래스 이용
+class Shape implements Box{
+    width:number;
+    height:number;
+    borderRadius:number;
+    backgroundColor:string;
+
+    constructor(
+        width:number,
+        height:number,
+        borderRadius:number,
+        backgroundColor:string,
+    ) {
+        this.width = width;
+        this.height = height;
+        this.borderRadius = borderRadius;
+        this.backgroundColor = backgroundColor;
     }
 }
 
-main();
+const boxShape = new Shape(10, 10, 0, 'blue');
+
+
+// 객체 변형
+box.borderRadius = 10;
+box['className'] = 'box rounded';
+
+const box1 = box;
+const box2 = Object.assign({}, box);
+const box4 = {...box, borderRadius: 10};
+const box3 = JSON.parse(JSON.stringify(box));
