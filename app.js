41cf5435cff6b8b6bb6fc8ac32ce6ce21134b662
dongduu <4012popo@naver.com>
2022-07-20 19:00:13 +0900

    feat: get api

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const ajax = new XMLHttpRequest();
+
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+
+ajax.open("GET", NEWS_URL, false);
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement("ul");
+
+for (i = 0; i < 10; i++) {
+  const li = document.createElement("li");
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById("root").appendChild(ul);
