3110b761c2ef0da3049b3de5f746b6c3ff89f850
dongduu <4012popo@naver.com>
2022-07-21 17:18:59 +0900

    feat: get news content url

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ajax = new XMLHttpRequest();
 
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENTS_URL = `http://api.hnpwa.com/v0/item/@id.json`;
 
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
@@ -8,11 +9,23 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
+window.addEventListener("hashchange", function () {
+  const id = location.hash.substr(1);
+
+  ajax.open("GET", CONTENTS_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  console.log(newsContent);
+});
+
 for (i = 0; i < 10; i++) {
   const li = document.createElement("li");
   const a = document.createElement("a");
-  a.href = "#";
+
+  a.href = `#${newsFeed[i].id}`;
   a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
   li.appendChild(a);
   ul.appendChild(li);
 }
