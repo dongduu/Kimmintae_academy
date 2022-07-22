193547b1b878188131035aa5d6b1aeb3f239c1ce
dongduu <4012popo@naver.com>
2022-07-22 23:09:13 +0900

    feat: router

--- app.js
+++ app.js
@@ -11,10 +11,24 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener("hashchange", function () {
+  newsList.push("<ul>");
+
+  for (i = 0; i < 10; i++) {
+    newsList.push(`<li>
+    <a href=#${newsFeed[i].id}>${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+  </li>`);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
   const id = location.hash.substr(1);
   const newsContent = getData(CONTENTS_URL.replace("@id", id));
 
@@ -23,18 +37,17 @@ window.addEventListener("hashchange", function () {
 
     <div><a href="#">목록으로</a></div>
   `;
-});
-
-const newsList = [];
-
-newsList.push("<ul>");
-
-for (i = 0; i < 10; i++) {
-  newsList.push(`<li>
-    <a href=#${newsFeed[i].id}>${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-  </li>`);
 }
 
-newsList.push("</ul>");
+function router() {
+  const routePath = location.hash;
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
 
-container.innerHTML = newsList.join("");
+window.addEventListener("hashchange", router);
+
+router();
