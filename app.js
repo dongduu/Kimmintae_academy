41b465b793a35219dce27376c3dd058d6e24a40f
dongduu <4012popo@naver.com>
2022-07-26 18:14:47 +0900

    feat: use recursion function

--- app.js
+++ app.js
@@ -104,12 +104,12 @@ function newsDetail() {
   </div>
   `;
 
-  function makeComment(comments) {
+  function makeComment(comments, called = 0) {
     const commentString = [];
 
     for (let i = 0; i < comments.length; i++) {
       commentString.push(`
-        <div style="padding-left: 40px;" class="mt-4">
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
           <div class="text-gray-400">
             <i class="fa fa-sort-up mr-2"></i>
             <strong>${comments[i].user}</strong> ${comments.time_ago}
@@ -117,6 +117,10 @@ function newsDetail() {
           <p class="text-gray-700">${comments[i].content}</p>
         </div>
       `);
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
     }
 
     return commentString.join("");
