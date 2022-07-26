614813e1695dd9488d6a004810ebe8ad1a79d9c5
dongduu <4012popo@naver.com>
2022-07-26 16:41:10 +0900

    feat: style newsfeed

--- app.js
+++ app.js
@@ -18,16 +18,25 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
   let template = `
-    <div class="container mx-auto p-4">
-      <h1>Hacker News</h1>
-      <ul>
-        {{__news_feed__}}
-      </ul>
-      <div>
-        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
-        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+    <div class="bg-gray-200 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+              <div class="flex justify-start">
+                <h1 class="font-extrabold">Hacker News</h1>
+              </div>
+              <div class="items-center justify-end">
+                <a href="#/page/{{__prev_page__}}" class="text-gray-500">Previous</a>
+                <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">Next</a>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 text-2xl text-gray-700">
+      {{__news_feed__}}
       </div>
     </div>
+    
   `;
 
   for (i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
